c669c49da6e6a156306f139093ec2429e831590d
shubham <msgtoshubham@gmail.com>
2022-11-21 18:02:24 +0530

    all features implemented and tests pass!

--- processes-shell/wish.c
+++ processes-shell/wish.c
@@ -42,19 +42,40 @@ void appendCharacter( char e , char **source , int* current) {
 
 void incrementArray(char *add , char ***source , int *current) {
     char **target = *source;
-    target = (char **)realloc(target , (*current)+1);
+    target = (char **)realloc(target , ((*current)+1)*sizeof(char *));
     target[(*current)-1] = add;
     (*current) = (*current) + 1;
     *source = target;
 }
 
+
+void incrementIndirectCharArray(char **add , char ****source , int *current) {
+    char ***target = *source;
+    target = (char ***)realloc(target , ((*current)+1)*sizeof(char **));
+    target[(*current)-1] = add;
+    (*current) = (*current) + 1;
+    *source = target;
+}
+
+
+void incrementIntegerArray(int add , int **source , int *current) {
+    int *target = *source;
+    target = (int *)realloc(target , ((*current)+1)*sizeof(int));
+    target[(*current)-1] = add;
+    (*current) = (*current) + 1;
+    *source = target;
+}
+
+
+
+
 char ** generateArguments(char **mainCommand, char *command, int *numberOfArguments , char ** redirectionFile, int *argumentError) {
     
-
     char ** args = (char **)malloc(sizeof(char *));
     int current = 1;
 
     int length = strlen(command);
+    char *curArg;
     for(int i=0;i<length;++i) {
 
 
@@ -89,6 +110,7 @@ char ** generateArguments(char **mainCommand, char *command, int *numberOfArgume
                 return args;
             }
             else {
+                // printf("breaking\n");
                 *redirectionFile = file;
                 break;
             }
@@ -96,7 +118,7 @@ char ** generateArguments(char **mainCommand, char *command, int *numberOfArgume
 
         *numberOfArguments = (*numberOfArguments) + 1;
         int j=i;
-        char *curArg = (char *)(malloc(sizeof(char)));
+        curArg = (char *)(malloc(sizeof(char)));
         curArg[0] = '\0';
         int curPos = 1;
 
@@ -116,36 +138,24 @@ char ** generateArguments(char **mainCommand, char *command, int *numberOfArgume
     }
 
     args[current-1] = NULL;
-
+    //printf("-%s-\n" , *mainCommand);
     // for(int i=0;i<current-1;++i) {
     //     printf("%d , %s--\n" ,strlen(args[i]), args[i]);
     // }
 
-
     return args;
 
 }
-void executeCommand(char *destination, char **args , char *redirectionFile) {
-    int rc = fork();
 
-    if(rc == 0) {
 
-        if(redirectionFile != NULL) {
-            int fd1 = creat(redirectionFile , 0644);
-            dup2(fd1, STDOUT_FILENO);
-            close(fd1);
-        }
 
-        execv(destination , args);
-    }
-    else {
-        wait(NULL);
-    }
-}
 
-char * findSearchPath(char *mainCommand , char **searchPaths , int len) {
-    
-    for(int i=0; i<len; ++i) {
+char * findSearchPath(char *mainCommand , char **searchPaths) {
+
+    if(strlen(mainCommand) == 0)
+        return NULL;
+    int i=0;
+    while(searchPaths[i] != NULL) {
         char *destination = (char *)(malloc(strlen(searchPaths[i])+1));
         strcpy(destination , searchPaths[i]);
         concatenate(&destination , "/");
@@ -154,6 +164,7 @@ char * findSearchPath(char *mainCommand , char **searchPaths , int len) {
         if(access(destination , X_OK) == 0) {
             return destination;
         }
+        i++;
     }
 
     return NULL;
@@ -186,6 +197,152 @@ char *readCommand(FILE ** fp , int *error) {
     return another;
 
 }
+
+char * formatInput(char *command) {
+
+    char *cleanedCommand = (char *)malloc(sizeof(char));
+    cleanedCommand[0] = 0;
+    int position = 1;
+
+    int index= 0;
+    int length = strlen(command);
+    while(index < length) {
+        if((command[index] != ' ') && (command[index] != '\t')) {
+            appendCharacter(command[index] , &cleanedCommand , &position);
+            index++;
+        }
+        else {
+
+            int toAppend = ((index != 0) ? 1 : 0);
+            while((index < length) && ((command[index] == ' ') || (command[index] == '\t'))) {
+                index++;
+            }
+
+            if(index == length) {
+                toAppend = 0;
+            }
+
+            if(toAppend) {
+                appendCharacter(' ' , &cleanedCommand , &position);
+            }
+        }
+    }
+
+    return cleanedCommand;
+}
+
+
+char ** generateParallelCommandList(char *command) {
+
+    char ** args = (char **)malloc(sizeof(char *));
+    int current = 1;
+
+    int length = strlen(command);
+
+    for(int i=0; i<length; ++i) {
+        if((command[i] == '&') || (i==0)) {
+            char *tempCommand = (char *)malloc(sizeof(char));
+            tempCommand[0] = '\0';
+            int position  = 1;
+            int j=i+1;
+            if(command[i] != '&') {
+                j =i;
+            }
+
+            while((j<length) && (command[j] != '&')) {
+                appendCharacter(command[j] , &tempCommand , &position);
+                j++;    
+            }
+
+            incrementArray(tempCommand, &args, &current);
+            
+            if(j==length)
+                break;
+            i = --j;
+        }
+    }
+
+    args[current-1] = NULL;
+    return args;
+}   
+
+
+
+void executeCommands(char ***searchPathsSource, char **mainCommands , char ***arguments , char **redirectionFiles , int *numberOfArgument, int * argumentErrors , int numberOfCommands) {
+    
+    int *pidList = (int *)malloc(sizeof(int *));
+    int size = 1;
+
+    for(int i=0; i<numberOfCommands; ++i) {
+        char *mainCommand = mainCommands[i];
+        char **searchPaths = *searchPathsSource;
+        char **args = arguments[i];
+        int numberOfArguments = numberOfArgument[i];
+        int length;
+        char *redirectionFile = redirectionFiles[i];
+        int argumentError = argumentErrors[i];
+
+        if(argumentError == 1) {
+            printError();
+            continue;
+        }
+
+        if(strcmp(mainCommand , "exit") == 0) {
+            if(numberOfArguments != 1) {
+                printError();
+                exit(0);
+            }
+            exit(0);
+        
+        }
+        else if(strcmp(mainCommand , "cd") == 0) {
+            if(numberOfArguments != 2) {
+                printError();
+                exit(0);
+            }
+            chdir(args[1]);
+        }
+        else if(strcmp(mainCommand, "path") == 0) {
+            searchPaths = realloc(searchPaths , sizeof(char *)*(numberOfArguments));
+            length = numberOfArguments - 1;
+            for(int i=0;i<numberOfArguments-1;++i) {
+                searchPaths[i] = args[i+1];
+            }  
+            searchPaths[numberOfArguments-1] = NULL;
+            *searchPathsSource = searchPaths;
+        }
+        else {
+            // printf("executing find search path\n");
+            char *destination = findSearchPath(mainCommand , searchPaths);
+            if(destination == NULL) {
+                printError();
+                continue;
+            }
+            else {
+               int rc = fork();
+               if(rc == 0) {
+
+                    if(redirectionFile != NULL) {
+                        int fd1 = creat(redirectionFile , 0644) ;
+                        dup2(fd1, STDOUT_FILENO);
+                        close(fd1);
+                    }
+                    execv(destination , args);
+               }
+               else {
+                incrementIntegerArray(rc , &pidList , &size);
+               }
+            }
+        }
+    }
+
+
+
+    for(int i=0;i<size-1;++i) {
+        waitpid(pidList[i] , NULL , 0);
+    }
+}
+
 int main(int argc , char *argv[]) {
 
 
@@ -205,9 +362,10 @@ int main(int argc , char *argv[]) {
         }
     }
 
-    char ** searchPaths = (char **)malloc(sizeof(char *));
-    int length = 1;
+    char ** searchPaths = (char **)malloc(sizeof(char *)*2);
+
     searchPaths[0] = "/bin";
+    searchPaths[1] = NULL;
     while(1) {
         
         if(argc == 1)
@@ -216,53 +374,60 @@ int main(int argc , char *argv[]) {
 
         int eof = 0;
         char *command = ((argc == 1) ? parseCommand() : readCommand(&fp , &eof));
+
+
         if(eof == -1) {
             break;
         }
-        int numberOfArguments = 0; 
-        char *mainCommand = (char *)malloc(sizeof(char));
-        char *redirectionFile = NULL;
 
-        int argumentError  = 0;
-        char **args = generateArguments(&mainCommand, command, &numberOfArguments, &redirectionFile , &argumentError);
+        char **rawCommandList = generateParallelCommandList(command);
 
-        if(argumentError == 1) {
-            printError();
-            exit(0);
-        }
+        char **mainCommands = (char **)malloc(sizeof(char *));
+        int commandSize = 1;
+        char ***arguments = (char ***)malloc(sizeof(char **));
+        int argumentSize = 1;
+        int *numberOfArguments = (int *)malloc(sizeof(int *));
+        int numberOfArgumentsSize = 1;
 
-        if(strcmp(mainCommand , "exit") == 0) {
-            if(numberOfArguments != 1) {
-                printError();
-                exit(0);
-            }
-            exit(0);
-        
-        }
-        else if(strcmp(mainCommand , "cd") == 0) {
-            if(numberOfArguments != 2) {
-                printError();
-                exit(0);
-            }
-            chdir(args[1]);
-        }
-        else if(strcmp(mainCommand, "path") == 0) {
-            searchPaths = realloc(searchPaths , sizeof(char *)*(numberOfArguments-1));
-            length = numberOfArguments - 1;
-            for(int i=0;i<numberOfArguments-1;++i) {
-                searchPaths[i] = args[i+1];
-            }  
-        }
-        else {
-            char *destination = findSearchPath(mainCommand , searchPaths, length);
-            if(destination == NULL) {
-                printError();
+        char **redirectionFiles = (char **)malloc(sizeof(char *));
+        int redirectFileSize = 1;
+        int *argumentErrors = (int *)malloc(sizeof(int *));
+        int argumentErrorsSize = 1;    
+
+
+        int ind = 0;
+        while(rawCommandList[ind] != NULL) {
+            //printf("%s\n"  ,rawCommandList[ind]);
+            rawCommandList[ind] = formatInput(rawCommandList[ind]);
+            //printf("%s\n", rawCommandList[ind]);
+            if(strlen(rawCommandList[ind]) == 0) {
+                ind++;
                 continue;
             }
-            else {
-                executeCommand(destination, args, redirectionFile);
-            }
-        }
+
+            int numberOfArgument = 0; 
+            char *mainCommand = (char *)malloc(sizeof(char));
+            mainCommand[0] = '\0';
+            char *redirectionFile = NULL;
+            int argumentError  = 0;
+            char **args = generateArguments(&mainCommand, rawCommandList[ind], &numberOfArgument, &redirectionFile , &argumentError);
+            //printf("-%s-\n" , mainCommand);
+
+            incrementArray(mainCommand , &mainCommands , &commandSize);
+            incrementIndirectCharArray(args , &arguments, &argumentSize);
+            incrementArray(redirectionFile , &redirectionFiles , &redirectFileSize);
+            incrementIntegerArray(numberOfArgument , &numberOfArguments , &numberOfArgumentsSize);
+            incrementIntegerArray(argumentError , &argumentErrors , &argumentErrorsSize);
+
+
+            ind++;
+        }   
+
+
+        //printf("-%s- %d\n" , mainCommands[0] , commandSize);
+        int numberOfCommands = commandSize-1;
+    
+        executeCommands(&searchPaths , mainCommands , arguments , redirectionFiles , numberOfArguments , argumentErrors , numberOfCommands);
     }
     return 0;
 }
